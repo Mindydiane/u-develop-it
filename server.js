814775ce7d62f12eb1fb4daa3e1386cacb5e8979
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-07-22 17:31:52 -0500

    queried the DB to test the connection w/rows

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,7 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (Not Found)
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your SQL password
+        password: '$0Met4i#9R@d',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Default response for any other request (Not Found), catchall route
 app.use((req, res) => {
     res.status(404).end();
 });
